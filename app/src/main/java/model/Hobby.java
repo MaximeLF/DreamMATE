3fbf48206fba5674efbdfc2c3e3403110e9fba42
Luã Streit <streitlua@gmail.com>
2019-03-24 18:43:07 +0100

    Skeleton of login and register done

---
+++ app/src/main/java/model/Hobby.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Hobby {
+
+    enum Type { sports, games, music, cooking, nature, partying, exploring, cultural};
+
+    public Type type;
+    public String description;
+
+}
