eeee897c4b6c7550859325baf97aec02117f1817
Lua Streit <streitlua@gmail.com>
2019-04-09 15:37:32 +0200

    Written code to fetch country list from server

---
+++ app/src/main/java/tasks/GetCountryListTask.java
@@ -0,0 +1,91 @@
+package tasks;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.dreammate.R;
+
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class GetCountryListTask extends AsyncTask<Void, Void, List<String>> {
+    private WeakReference<AppCompatActivity> actWeakRef;
+
+    public GetCountryListTask(AppCompatActivity act) {
+        actWeakRef = new WeakReference<>(act);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected List<String> doInBackground(Void... voids)
+    {
+        if (actWeakRef != null)
+        {
+            Uri.Builder builder = new Uri.Builder();
+            builder.scheme("https");
+            String server = actWeakRef.get().getApplicationContext().getResources().getString(R.string.server_url);
+            builder.authority(server);
+            builder.appendPath("dev");
+            builder.appendPath("countries_list");
+
+            try {
+                URL url = new URL(builder.build().toString());
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestProperty("Content-Type", "application/json");
+
+                connection.setRequestMethod("GET");
+
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK)
+                {
+                    InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+                    Scanner s = new Scanner(reader).useDelimiter("\\A");
+
+                    String answer = s.hasNext() ? s.next() : "";
+
+                    reader.close();
+                    connection.disconnect();
+
+                    Log.d("lua", "Received : " + answer);
+
+                    answer = answer.substring(1, answer.length() - 1); // remove '[' at the beginning and ']' at the end
+                    List<String> countries = new ArrayList<>();
+                    String[] words = answer.split(",");
+
+                    for (int i = 0; i < words.length; i++) {
+                        countries.add(words[i].trim());
+                    }
+
+                    return countries;
+                }
+                else {
+                    Log.d("lua", "Response code was NOT OK");
+                }
+            }
+            catch (Exception e)
+            {
+                Log.e("lua", e.toString());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(List<String> countries) {
+        super.onPostExecute(countries);
+        if (actWeakRef != null) {
+           // actWeakRef.get(); // PUT HERE THE CODE THAT PUTS THE GIVEN LIST IN THE ACTIVITY
+        }
+    }
+}
